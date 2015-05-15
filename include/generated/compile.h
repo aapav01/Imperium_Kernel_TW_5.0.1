a1a2d9f70549e5d5f2fa68d02f7011c77b75b99f
Apavayan <info@apavayan.com>
2015-05-15 15:05:11 +0530

    chmod 777

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Aug 5 16:27:34 KST 2014"
+#define LINUX_COMPILE_BY "t0203kim"
+#define LINUX_COMPILE_HOST "t0203"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
